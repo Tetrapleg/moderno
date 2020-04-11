434c2922a32d7a81dc1719efad966149c50f3028
Tetrapleg <rufmama@yandex.ru>
2020-04-11 17:35:24 +0300

    added new_item page

--- app/js/main.js
+++ app/js/main.js
@@ -13,8 +13,38 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+        
+
+    $('.icon-th-list').on('click', function(){
+        $('.products__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.products__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
     
-    var mixer = mixitup('.products__inner-box');
+    // var mixer = mixitup('.products__inner-box');
+
+    $(window).on("load", function () {
+        if ($('.products__inner-box').length) {
+            var mixer = mixitup('.products__inner-box');
+          }
+    });
+
+
 
 
 });
